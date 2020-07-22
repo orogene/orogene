782e256eb73f9009674cdc65ae25ea1175fe6709
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-07-22 01:13:17 -0700

    allow "unused mut" in AsyncWrite impls
    Removing the "mut" actually breaks the tests, while leaving the "mut"
    in place breaks the doc builds. Marking the "unused mut" as ignored
    side-steps the issue so both docs and tests complete successfully.

--- crates/cacache/src/content/write.rs
+++ crates/cacache/src/content/write.rs
@@ -216,7 +216,7 @@ impl AsyncWriter {
 
 impl AsyncWrite for AsyncWriter {
     fn poll_write(
-        mut self: Pin<&mut Self>,
+        #[allow(unused_mut)] mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<std::io::Result<usize>> {
@@ -273,7 +273,10 @@ impl AsyncWrite for AsyncWriter {
         }
     }
 
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+    fn poll_flush(
+        #[allow(unused_mut)] mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<std::io::Result<()>> {
         let state = &mut *self.0.lock().unwrap();
 
         loop {
@@ -313,7 +316,10 @@ impl AsyncWrite for AsyncWriter {
         }
     }
 
-    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+    fn poll_close(
+        #[allow(unused_mut)] mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<std::io::Result<()>> {
         let state = &mut *self.0.lock().unwrap();
 
         loop {
