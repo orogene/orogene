c637e1db215eef25a60268e066f993b1c1fea1ad
Felipe Sere <felipesere@gmail.com>
2020-08-03 23:59:57 +0200

    Use node compatible version ranges

--- crates/package-arg/tests/npm.rs
+++ crates/package-arg/tests/npm.rs
@@ -6,6 +6,12 @@ fn ppa(input: &str) -> Result<PackageArg> {
     input.parse()
 }
 
+fn version_req(input: &str) -> Option<VersionReq> {
+    Some(VersionReq::Range(
+        semver::VersionReq::parse_compat(input, semver::Compat::Node).unwrap(),
+    ))
+}
+
 #[test]
 fn npm_pkg_basic() -> Result<()> {
     let res = ppa("hello-world")?;
@@ -143,6 +149,90 @@ fn npm_pkg_prefixed_with_req() -> Result<()> {
             requested: Some(VersionReq::Version(
                 semver::Version::parse("1.2.3").unwrap()
             ))
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn odd_npm_example_with_prerelease() -> Result<()> {
+    let res = ppa("world@>1.1.0-beta-10")?;
+    assert_eq!(
+        res,
+        PackageArg::Npm {
+            scope: None,
+            name: "world".into(),
+            requested: version_req(">1.1.0-beta-10"),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn approximately_equivalent_version() -> Result<()> {
+    let res = ppa("world@~1.1.0")?;
+    assert_eq!(
+        res,
+        PackageArg::Npm {
+            scope: None,
+            name: "world".into(),
+            requested: version_req("~1.1.0"),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn compatible_equivalent_version() -> Result<()> {
+    let res = ppa("world@^1.1.0")?;
+    assert_eq!(
+        res,
+        PackageArg::Npm {
+            scope: None,
+            name: "world".into(),
+            requested: version_req("^1.1.0"),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn x_version() -> Result<()> {
+    let res = ppa("world@1.1.x")?;
+    assert_eq!(
+        res,
+        PackageArg::Npm {
+            scope: None,
+            name: "world".into(),
+            requested: version_req("1.1.x"),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn hyphen_version_range() -> Result<()> {
+    let res = ppa("world@1.5.0 - 2.1.0")?;
+    assert_eq!(
+        res,
+        PackageArg::Npm {
+            scope: None,
+            name: "world".into(),
+            requested: version_req("1.5.0 - 2.1.0"),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn alternate_version_ranges() -> Result<()> {
+    let res = ppa("world@1.5.0 - 2.1.0 || 2.3.x")?;
+    assert_eq!(
+        res,
+        PackageArg::Npm {
+            scope: None,
+            name: "world".into(),
+            requested: version_req("1.5.0 - 2.1.0 || 2.3.x"),
         }
     );
     Ok(())
