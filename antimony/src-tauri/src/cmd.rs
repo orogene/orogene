dfc84811265165475308901514320d5ee8634097
Kat Marchán <kzm@zkat.tech>
2020-08-23 15:48:44 -0700

    poking at tauri app a bit more

--- antimony/src-tauri/src/cmd.rs
+++ antimony/src-tauri/src/cmd.rs
@@ -3,15 +3,15 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 #[serde(tag = "cmd", rename_all = "camelCase")]
 pub enum Cmd {
-  // your custom commands
-  // multiple arguments are allowed
-  // note that rename_all = "camelCase": you need to use "myCustomCommand" on JS
-  BlockingEcho {
-    msg: String,
-  },
-  AsyncEcho {
-    msg: String,
-    callback: String,
-    error: String,
-  },
+    // your custom commands
+    // multiple arguments are allowed
+    // note that rename_all = "camelCase": you need to use "myCustomCommand" on JS
+    BlockingEcho {
+        msg: String,
+    },
+    AsyncEcho {
+        msg: String,
+        callback: String,
+        error: String,
+    },
 }
