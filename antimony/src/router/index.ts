94a6cfe77c4ee6fa3b501b55bdd8cea18ef50b53
BenoitRanque <ranque.benoit@gmail.com>
2020-08-24 18:31:56 -0400

    Fixed and vendored vue-router (#61)
    * Added vue router, prettier
    
    [FIX] #49
    
    * add prettier action
    
    * Added vue router, prettier
    
    [FIX] #49
    
    * add prettier action
    
    * Added vue router, prettier
    
    [FIX] #49
    
    * Added vue router, prettier
    
    [FIX] #49
    
    * Use local vue-router next, until fix goes live on npm

--- antimony/src/router/index.ts
+++ antimony/src/router/index.ts
@@ -1,4 +1,11 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+// use out own vue-router for now, until the edge fix goes live on npm
+import {
+  createRouter,
+  createWebHashHistory,
+  RouteRecordRaw
+  // @ts-ignore expect error here since this is not a TS file. This is until the next router version
+} from '@/assets/vue-router.esm.js';
+// import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 import Home from '../views/Home.vue';
 
