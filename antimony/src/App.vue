cd3ecd824e52c956162a3b28225d5c7a6eb56212
BenoitRanque <ranque.benoit@gmail.com>
2020-08-23 23:08:26 -0400

    Added vue router, prettier
    [FIX] #49

--- antimony/src/App.vue
+++ antimony/src/App.vue
@@ -1,44 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <button @click="sayHelloBlocking()">Hello Blocking</button>
-    <button @click="sayHelloAsync()">Hello Async</button>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import { invoke, promisified } from 'tauri/api/tauri'
-
-interface AsyncEchoResponse {
-  msg: string
-}
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  methods: {
-    sayHelloBlocking () {
-      invoke({
-        cmd: 'blockingEcho',
-        msg: 'blocking hello from js!'
-      })
-    },
-    sayHelloAsync () {
-      promisified<AsyncEchoResponse>({
-        cmd: 'asyncEcho',
-        msg: 'async hello from js!'
-      }).then(result => {
-        alert(result.msg)
-      }).catch(console.error)
-    }
-  }
-});
-</script>
 
 <style>
 #app {
@@ -47,6 +15,18 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
