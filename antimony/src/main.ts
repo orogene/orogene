86df824dc12b9cbc967b0c4b3c325cc2b3eb1029
Benoit Ranque <ranque.benoit@gmail.com>
2020-08-30 14:58:10 -0400

    added vue-fontawesome

--- antimony/src/main.ts
+++ antimony/src/main.ts
@@ -2,4 +2,23 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// @ts-ignore expect error here since this is not a TS file. This is until the currently pending PR on that repo is accepted and merged.
+import { FontAwesomeIcon } from './assets/temp-deps/vue-fontawesome';
+
+library.add(faUserSecret);
+
+const app = createApp(App);
+
+/*
+  register font-awesome-icon component globally.
+  example usage:
+
+  ```html
+    <font-awesome-icon icon="user-secret" />
+  ```
+ */
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router);
+app.mount('#app');
