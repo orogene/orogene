95edcbd79ad1aa2fedc85af4ac9301076932ea78
Kat Marchán <kzm@zkat.tech>
2020-11-14 20:57:44 -0800

    some setup for eventual pretty printers

--- crates/oro-diagnostics/src/lib.rs
+++ crates/oro-diagnostics/src/lib.rs
@@ -9,6 +9,30 @@ pub trait Diagnostic: std::error::Error + Send + Sync {
     fn diagnostic_path(&self) -> String {
         format!("{}::{}", self.category().prefix(), self.subpath())
     }
+    fn pretty_print(&self) -> String {
+        use DiagnosticCategory::*;
+        let mut output = String::new();
+        output.push_str("Code: ");
+        output.push_str(&self.diagnostic_path()[..]);
+        output.push_str("\n\n");
+        output.push_str(&self.to_string()[..]);
+        output.push_str(&match self.category() {
+            Misc => "".into(),
+            Net { ref host, ref url  } => {
+                if let Some(url) = url {
+                    format!("\n\nurl: {}", url)
+                } else {
+                    format!("\n\nhost: {}", host)
+                }
+            },
+            Fs { .. } => "something happened with the filesystem".into(),
+            Parse { .. } => "something happened while parsing".into(),
+        }[..]);
+        if let Some(advice) = self.advice() {
+            output.push_str(&format!("\n\nhelp: {}", advice)[..])
+        }
+        output
+    }
 }
 
 // This is needed so Box<dyn Diagnostic> is correctly treated as an Error.
