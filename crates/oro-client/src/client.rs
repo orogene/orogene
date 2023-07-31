87facfe44a14a61e94ee50c0a5f64724b065bdd8
cin <eryue0220@gmail.com>
2023-08-01 00:40:20 +0800

    feat(proxi): support proxy configuration (#283)
    Fixes: https://github.com/orogene/orogene/issues/223

--- crates/oro-client/src/client.rs
+++ crates/oro-client/src/client.rs
@@ -8,14 +8,25 @@ use reqwest::Client;
 #[cfg(not(target_arch = "wasm32"))]
 use reqwest::ClientBuilder;
 #[cfg(not(target_arch = "wasm32"))]
+use reqwest::{NoProxy, Proxy};
+#[cfg(not(target_arch = "wasm32"))]
 use reqwest_middleware::ClientWithMiddleware;
 use url::Url;
+
+#[cfg(not(target_arch = "wasm32"))]
+use crate::OroClientError;
 
 #[derive(Clone, Debug)]
 pub struct OroClientBuilder {
     registry: Url,
     #[cfg(not(target_arch = "wasm32"))]
     cache: Option<PathBuf>,
+    #[cfg(not(target_arch = "wasm32"))]
+    proxy: bool,
+    #[cfg(not(target_arch = "wasm32"))]
+    proxy_url: Option<Proxy>,
+    #[cfg(not(target_arch = "wasm32"))]
+    no_proxy_domain: Option<String>,
 }
 
 impl Default for OroClientBuilder {
@@ -24,6 +35,12 @@ impl Default for OroClientBuilder {
             registry: Url::parse("https://registry.npmjs.org").unwrap(),
             #[cfg(not(target_arch = "wasm32"))]
             cache: None,
+            #[cfg(not(target_arch = "wasm32"))]
+            proxy: false,
+            #[cfg(not(target_arch = "wasm32"))]
+            proxy_url: None,
+            #[cfg(not(target_arch = "wasm32"))]
+            no_proxy_domain: None,
         }
     }
 }
@@ -44,17 +61,61 @@ impl OroClientBuilder {
         self
     }
 
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn set_proxy(mut self, proxy: bool) -> Self {
+        self.proxy = proxy;
+        self
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn set_proxy_url(mut self, proxy_url: impl AsRef<str>) -> Result<Self, OroClientError> {
+        match Url::parse(proxy_url.as_ref()) {
+            Ok(url_info) => {
+                let username = url_info.username();
+                let password = url_info.password();
+                let mut proxy = Proxy::all(url_info.as_ref())?;
+
+                if let Some(password_str) = password {
+                    proxy = proxy.basic_auth(username, password_str);
+                }
+
+                proxy = proxy.no_proxy(self.get_no_proxy());
+                self.proxy_url = Some(proxy);
+                self.proxy = true;
+                Ok(self)
+            }
+            Err(e) => Err(OroClientError::UrlParseError(e)),
+        }
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn set_no_proxy(mut self, no_proxy_domain: impl AsRef<str>) -> Self {
+        self.no_proxy_domain = Some(no_proxy_domain.as_ref().into());
+        self
+    }
+
     pub fn build(self) -> OroClient {
         #[cfg(target_arch = "wasm32")]
         let client_uncached = Client::new();
 
         #[cfg(not(target_arch = "wasm32"))]
-        let client_uncached = ClientBuilder::new()
+        let mut client_core = ClientBuilder::new()
             .user_agent("orogene")
             .pool_max_idle_per_host(20)
-            .timeout(std::time::Duration::from_secs(60 * 5))
-            .build()
-            .expect("Failed to build HTTP client.");
+            .timeout(std::time::Duration::from_secs(60 * 5));
+
+        #[cfg(not(target_arch = "wasm32"))]
+        if let Some(url) = self.proxy_url {
+            client_core = client_core.proxy(url);
+        }
+
+        #[cfg(not(target_arch = "wasm32"))]
+        if !self.proxy {
+            client_core = client_core.no_proxy();
+        }
+
+        #[cfg(not(target_arch = "wasm32"))]
+        let client_uncached = client_core.build().expect("Fail to build HTTP client.");
 
         #[cfg(not(target_arch = "wasm32"))]
         let mut client_builder = reqwest_middleware::ClientBuilder::new(client_uncached.clone());
@@ -79,6 +140,17 @@ impl OroClientBuilder {
             client: client_uncached.clone(),
             client_uncached,
         }
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn get_no_proxy(&self) -> Option<NoProxy> {
+        if let Some(ref no_proxy_conf) = self.no_proxy_domain {
+            if !no_proxy_conf.is_empty() {
+                return NoProxy::from_string(no_proxy_conf);
+            }
+        }
+
+        NoProxy::from_env().or(None)
     }
 }
 
