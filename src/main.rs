2386e506c30003fa16123f9934d3f7b814f28c15
Sam Gammon <sam@elide.ventures>
2024-07-07 21:32:11 -0700

    feat: add optional `mimalloc` feature
    Adds an optional feature to use `mimalloc` as the
    default allocator
    
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use miette::Result;
 use orogene::Orogene;
 
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[async_std::main]
 async fn main() -> Result<()> {
     Ok(Orogene::load().await?)
